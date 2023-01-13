f66dde40f5fce4896872f042df1ee04c6287f028
ArtemYU <uhnovets@gmail.com>
2023-01-13 09:27:05 +0300

    add Examples005

---
+++ Examples005_ConditionElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower()=="маша") // если использовать username.ToLower(), то уйдет проблема того как пльзователь введет свое имя
+// username.ToLower переводит все символы в нижний регистр
+{
+    Console.WriteLine("Ура, это же МАША!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+    Console.WriteLine("!");
+}
